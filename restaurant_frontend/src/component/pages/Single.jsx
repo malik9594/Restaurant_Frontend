4a524fcd90e95d25a448bab07e0fa201d28c040c
Hassan Raza Malik <155219053+malik9594SysReforms@users.noreply.github.com>
2024-10-13 16:56:01 +0500

    new fixes

--- restaurant_frontend/src/component/pages/Single.jsx
+++ restaurant_frontend/src/component/pages/Single.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Edit from '../img/edit.png';
 import Delete from '../img/delete.png';
 import { Link } from 'react-router-dom';
+import Menu from '../Menu';
 const Single = () => {
   return (
     <div className="SingleTop">
@@ -14,14 +15,18 @@ const Single = () => {
             <p>Posted 2 days ago</p>
           </div>
           <div className="edit">
-          <Link to={`/write?edit=2`}>  <img src={Edit}/></Link>
-            <img src={Delete}/>
+            <Link to={`/write?edit=2`}>  <img src={Edit} /></Link>
+            <img src={Delete} />
           </div>
         </div>
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ea et soluta, possimus, ab officia voluptates nisi natus rem unde quia. Impedit, officia.
+        </h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias ut in reiciendis cupiditate rem laborum similique doloribus voluptate voluptas quis enim, atque molestiae. Ipsam magni labore vero assumenda amet dolore aliquid voluptate asperiores incidunt ea accusantium, commodi vitae voluptas nam voluptatum perspiciatis nemo omnis quam consectetur dolores, obcaecati repellat aliquam.<br />
+          <br />
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus magnam quis assumenda asperiores? Perferendis sed autem, voluptatibus veritatis sint iure optio placeat officia, cum fuga illum commodi inventore? Quidem, quos nulla eaque tempora tempore nihil!</p></p>
       </div>
-      <div className="menu">
-
-      </div>
+      <Menu/>
     </div>
   )
 }
