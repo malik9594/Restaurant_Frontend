2ac5c3d54fbf60a0530645a44b88ab31012ed9f1
Hassan Raza Malik <155219053+malik9594SysReforms@users.noreply.github.com>
2024-10-14 15:21:52 +0500

    new fixes

--- restaurant_frontend/src/component/pages/Write.jsx
+++ restaurant_frontend/src/component/pages/Write.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const Write = () => {
+  const [value, setValue] = useState('');
   return (
-    <div>
-            This is Writer
-
+    <div className="">
+         <div className="write1">
+      <div className="content">
+        <input type="text" placeholder='Title'/>
+        <div className="editor-container">
+        <ReactQuill theme="snow" value={value} onChange={setValue} />
+        </div>
+      </div>
+      <div className="menu">
+        <div className="item">
+          i1
+        </div>
+        <div className="item">i2</div>
+      </div>
     </div>
+    </div>
+ 
   )
 }
 
