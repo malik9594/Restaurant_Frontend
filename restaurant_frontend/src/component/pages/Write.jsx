5ce7bc0812117aa2ee6544cda99bbdcefaf19a0a
Hassan Raza Malik <155219053+malik9594SysReforms@users.noreply.github.com>
2024-10-15 15:42:05 +0500

    new fixes

--- restaurant_frontend/src/component/pages/Write.jsx
+++ restaurant_frontend/src/component/pages/Write.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import EditorToolbar, {modules, formats} from "./EditorToolbar"
 
 const Write = () => {
   const [value, setValue] = useState('');
@@ -8,16 +9,47 @@ const Write = () => {
     <div className="">
          <div className="write1">
       <div className="content">
-        <input type="text" placeholder='Title'/>
+        <input className="input-title" type="text" placeholder='Title'/>
         <div className="editor-container">
-        <ReactQuill theme="snow" value={value} onChange={setValue} />
+          <EditorToolbar/>
+        <ReactQuill theme="snow" modules={modules} formats={formats} className="editor" value={value} onChange={setValue} />
         </div>
       </div>
       <div className="menu">
         <div className="item">
-          i1
+          <h1>Publish</h1>
+          <span><b>Status:</b>
+          Draft
+          </span>
+          <span><b>Visibility:</b>
+          Public
+          </span>
+          <input style={{display:"none"}} type="file" name="" id="file"/>
+          <label className="file" htmlFor="file">Upload Image</label>
+        <div className="buttons">
+          <button>
+            Save as a draft
+          </button>
+          <button>
+            Update
+          </button>
         </div>
-        <div className="item">i2</div>
+        </div>
+        <div className="item1">
+          <h1>Category</h1>
+          <input type="radio" name="cat" value="art" id="art"/>
+          <label  htmlFor='art'>Art</label>
+          <input type="radio" name="cat" value="science" id="science"/>
+          <label htmlFor='science'>Science</label>
+          <input type="radio" name="cat" value="technology" id="technology"/>
+          <label htmlFor='technology'>Technology</label>
+          <input type="radio" name="cat" value="cinema" id="cinema"/>
+          <label htmlFor='cinema'>Cinema</label>
+          <input type="radio" name="cat" value="design" id="design"/>
+          <label htmlFor='design'>Design</label>
+          <input type="radio" name="cat" value="food" id="food"/>
+          <label htmlFor='food'>Food</label>
+        </div>
       </div>
     </div>
     </div>
