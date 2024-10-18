85e07643cc51b3d45056000e4f5f04a3ec10c0dd
Hassan Raza Malik <135107174+malik9594@users.noreply.github.com>
2024-10-18 17:28:45 +0500

    new fixes

--- restaurant_frontend/src/component/pages/Register.jsx
+++ restaurant_frontend/src/component/pages/Register.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username:"",
+    email:"",
+    password:"",
+  })
+
+
+  const handleChange=(e)=>{
+setInputs(prev=>({...prev,[e.target.name]:e.target.value}))
+  }
   return (
     <div className="auth">
       <h1>Register</h1>
       <form>
-        <input required type="text" placeholder="User Name"></input>
-        <input  required type="email" placeholder="Email"></input>
+        <input required type="text" placeholder="User Name" name="username" onChange={handleChange}></input>
+        <input  required type="email" placeholder="Email" name="email" onChange={handleChange}></input>
 
-        <input  required type="password" placeholder="Password"></input>
+        <input  required type="password" placeholder="Password" name="password" onChange={handleChange}></input>
         <button>Login</button>
         <p>This is an error!</p>
         <span>Don you have an account?<Link to={'/login'}>Login</Link></span>
